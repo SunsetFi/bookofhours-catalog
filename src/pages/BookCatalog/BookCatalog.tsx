90e4af2189fde68e517448980de84aed1ffe3e24
SunsetFi <sunsetfidev@gmail.com>
2023-08-27 15:24:20 -0700

    Data grid.  Jank, but it handles sorting for me.

--- src/pages/BookCatalog/BookCatalog.tsx
+++ src/pages/BookCatalog/BookCatalog.tsx
@@ -1,48 +1,219 @@
 import * as React from "react";
 import { useObservableState } from "observable-hooks";
-import { of as observableOf } from "rxjs";
-
 import {
-  Box,
-  Table,
-  TableHead,
-  TableBody,
-  TableRow,
-  TableCell,
-  Typography,
-} from "@mui/material";
+  Observable,
+  combineLatest,
+  of as observableOf,
+  map,
+  mergeMap,
+} from "rxjs";
 
+import { Box, TableRow, TableCell, Typography } from "@mui/material";
+import { DataGrid, GridColDef } from "@mui/x-data-grid";
+
+import { observeAll } from "@/observables";
 import { useDIDependency } from "@/container";
 
-import { useVisibleReadables } from "@/services/sh-monitor/hooks";
-import { ElementStackModel } from "@/services/sh-monitor";
+import { ElementStackModel, GameModel } from "@/services/sh-monitor";
 import { API } from "@/services/sh-api";
 
 import { RequireLegacy } from "@/components/RequireLegacy";
 
+interface TableBookItem {
+  id: string;
+  iconUrl: string;
+  label: string;
+  description: string;
+  location: string | null;
+  mastery: {
+    aspect: string;
+    value: number;
+    iconUrl: string;
+  } | null;
+}
+
+const columns: GridColDef<TableBookItem>[] = [
+  {
+    field: "iconUrl",
+    headerName: "Icon",
+    width: 90,
+    sortable: false,
+    disableColumnMenu: true,
+    disableExport: true,
+    disableReorder: true,
+    renderCell: ({ value }) => (
+      <Box
+        sx={{
+          width: "100%",
+          height: "100%",
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "center",
+          p: 2,
+        }}
+      >
+        <img src={value} style={{ maxWidth: "75px", maxHeight: "75px" }} />
+      </Box>
+    ),
+  },
+  {
+    field: "label",
+    headerName: "Name",
+    width: 200,
+  },
+  {
+    field: "location",
+    headerName: "Location",
+    width: 150,
+  },
+  {
+    field: "mastery",
+    headerName: "Mastery",
+    width: 150,
+    sortComparator: (
+      v1: TableBookItem["mastery"],
+      v2: TableBookItem["mastery"]
+    ) => {
+      if (v1 == null) {
+        return -1;
+      }
+
+      if (v2 == null) {
+        return 1;
+      }
+
+      return v1.value - v2.value;
+    },
+    renderCell: ({ value }) => {
+      if (value == null) {
+        return null;
+      }
+
+      return (
+        <Box
+          component="span"
+          sx={{
+            display: "flex",
+            flexDirection: "row",
+            justifyContent: "center",
+            alignItems: "center",
+            gap: 2,
+          }}
+        >
+          <img style={{ width: "50px" }} src={value.iconUrl} />
+          <Typography
+            style={{ whiteSpace: "nowrap" }}
+            component="span"
+            variant="h4"
+            color="text.secondary"
+          >
+            {value.value}
+          </Typography>
+        </Box>
+      );
+    },
+  },
+  {
+    field: "description",
+    headerName: "Description",
+    flex: 2,
+    renderCell: ({ value }) => (
+      <Box
+        sx={{ display: "flex", flexDirection: "column", alignItems: "center" }}
+      >
+        <Typography
+          variant="body2"
+          sx={{
+            width: "100%",
+            height: "100%",
+            whiteSpace: "break-spaces",
+          }}
+        >
+          {value}
+        </Typography>
+      </Box>
+    ),
+  },
+];
+
+function elementStackToItem(
+  api: API,
+  elementStack: ElementStackModel
+): Observable<TableBookItem> {
+  return combineLatest([
+    elementStack.label$,
+    elementStack.description$,
+    elementStack.parentTerrain$.pipe(
+      mergeMap((terrain) => terrain?.label$ ?? observableOf(null))
+    ),
+    elementStack.elementAspects$,
+  ]).pipe(
+    map(([label, description, location, aspects]) => {
+      const masteryKey = Object.keys(aspects).find((x) =>
+        x.startsWith("mystery.")
+      );
+
+      return {
+        id: elementStack.id,
+        iconUrl: elementStack.iconUrl,
+        label,
+        description,
+        location,
+        mastery:
+          masteryKey == null
+            ? null
+            : {
+                aspect: masteryKey,
+                value: aspects[masteryKey],
+                // FIXME: Get this from a model.
+                iconUrl: `${api.baseUrl}/api/compendium/elements/${masteryKey}/icon.png`,
+              },
+      };
+    })
+  );
+}
+
+function autoRowHeight() {
+  return "auto" as const;
+}
+
 const BookCatalog = () => {
-  const readableModels = useVisibleReadables();
+  const api = useDIDependency(API);
+  const model = useDIDependency(GameModel);
+  const readables$ = model.visibleReadables$.pipe(
+    map((readables) => readables.map((item) => elementStackToItem(api, item))),
+    observeAll()
+  );
+  const readables = useObservableState(readables$, []);
 
   return (
-    <Box>
+    <Box
+      sx={{
+        display: "flex",
+        flexDirection: "column",
+        width: "100%",
+        height: "100%",
+      }}
+    >
       <RequireLegacy />
-      <Table>
-        <TableHead>
-          <TableRow>
-            <TableCell></TableCell>
-            <TableCell>Element</TableCell>
-            <TableCell>Location</TableCell>
-            <TableCell>Mastery</TableCell>
-            <TableCell>Description</TableCell>
-          </TableRow>
-        </TableHead>
-        <TableBody>
-          {readableModels.map((book) => (
-            <BookRow key={book.id} book={book} />
-          ))}
-          <TableRow></TableRow>
-        </TableBody>
-      </Table>
+      <Typography
+        variant="h4"
+        sx={{ py: 2, width: "100%", textAlign: "center" }}
+      >
+        The Collected Works of Hush House
+      </Typography>
+      <Box
+        sx={{ width: "100%", height: "100%", minHeight: 0, overflow: "hidden" }}
+      >
+        <DataGrid
+          columns={columns}
+          rows={readables}
+          rowHeight={100}
+          // getRowHeight={autoRowHeight}
+          density="comfortable"
+          pageSizeOptions={[10, 25, 50]}
+        />
+      </Box>
     </Box>
   );
 };
