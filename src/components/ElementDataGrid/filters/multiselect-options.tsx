3f8427146eb197202a648e014b455b8c895ac602
SunsetFi <sunsetfidev@gmail.com>
2023-08-28 17:38:48 -0700

    Searchable select

--- src/components/ElementDataGrid/filters/multiselect-options.tsx
+++ src/components/ElementDataGrid/filters/multiselect-options.tsx
@@ -1,5 +1,7 @@
 import * as React from "react";
 
+import Box from "@mui/material/Box";
+import TextField from "@mui/material/TextField";
 import List from "@mui/material/List";
 import ListItem from "@mui/material/ListItem";
 import ListItemButton from "@mui/material/ListItemButton";
@@ -35,38 +37,52 @@ const MultiselectOptionsFilter = ({
   value: selectedValues,
   onChange,
 }: MultiselectOptionsFilterProps) => {
+  const [search, setSearch] = React.useState<string>("");
   return (
-    <List sx={{ maxHeight: "600px" }}>
-      {allowedValues.map((value) => (
-        <ListItem key={value} disablePadding>
-          <ListItemButton
-            dense
-            onClick={(e) => {
-              e.preventDefault();
-              e.stopPropagation();
+    <Box sx={{ display: "flex", flexDirection: "column", maxHeight: "600px" }}>
+      <TextField
+        sx={{ p: 1, mb: 1 }}
+        value={search}
+        onChange={(e) => setSearch(e.target.value)}
+      />
+      <List sx={{ overflow: "auto" }}>
+        {allowedValues
+          .filter(
+            (item) =>
+              search === "" ||
+              String(item).toLowerCase().includes(search.toLowerCase())
+          )
+          .map((value) => (
+            <ListItem key={value} disablePadding>
+              <ListItemButton
+                dense
+                onClick={(e) => {
+                  e.preventDefault();
+                  e.stopPropagation();
 
-              let newValue = selectedValues;
-              if (newValue.includes(value)) {
-                newValue = newValue.filter((x) => x !== value);
-              } else {
-                newValue = [...newValue, value];
-              }
+                  let newValue = selectedValues;
+                  if (newValue.includes(value)) {
+                    newValue = newValue.filter((x) => x !== value);
+                  } else {
+                    newValue = [...newValue, value];
+                  }
 
-              onChange(newValue);
-            }}
-          >
-            <ListItemIcon>
-              <Checkbox
-                edge="start"
-                checked={selectedValues.includes(value)}
-                tabIndex={-1}
-                disableRipple
-              />
-            </ListItemIcon>
-            <ListItemText primary={value} />
-          </ListItemButton>
-        </ListItem>
-      ))}
-    </List>
+                  onChange(newValue);
+                }}
+              >
+                <ListItemIcon>
+                  <Checkbox
+                    edge="start"
+                    checked={selectedValues.includes(value)}
+                    tabIndex={-1}
+                    disableRipple
+                  />
+                </ListItemIcon>
+                <ListItemText primary={value} />
+              </ListItemButton>
+            </ListItem>
+          ))}
+      </List>
+    </Box>
   );
 };
