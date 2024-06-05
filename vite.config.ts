3bac36e1e59e08fca67d463d9ffb442b0977b9ba
SunsetFi <sunsetfidev@gmail.com>
2024-06-05 12:33:46 -0700

    Better chunking

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,13 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import checker from "vite-plugin-checker";
 
+const NamedChunks = {
+  react: ["react", "react-dom"],
+  material: ["@emotion/styled", "@mui/material", "@mui/icons-material"],
+  rxjs: ["rxjs", "scheduler"],
+  lodash: ["lodash"],
+};
+
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), checker({ typescript: true })],
@@ -17,15 +24,18 @@ export default defineConfig({
   build: {
     rollupOptions: {
       output: {
-        manualChunks: {
-          react: ["react", "react-dom"],
-          ui: [
-            "@emotion/styled",
-            "@mui/material",
-            "@mui/icons-material",
-            "@tanstack/react-table",
-            "@tanstack/react-virtual",
-          ],
+        manualChunks: (id) => {
+          if (id.includes("node_modules")) {
+            // Check explicit groups
+            for (const [name, modules] of Object.entries(NamedChunks)) {
+              if (modules.some((x) => id.includes(`/node_modules/${x}/`))) {
+                return name;
+              }
+            }
+
+            // Everything else from node_modules is vendor
+            return "vendor";
+          }
         },
       },
     },
