8af90ca222c2d82a8d1ea04d4592d10d75a0906e
SunsetFi <sunsetfidev@gmail.com>
2025-09-27 10:10:52 -0700

    Fix version / package import

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react";
 import checker from "vite-plugin-checker";
 import svgr from "vite-plugin-svgr";
 
-import * as pkg from "./package.json" with { type: "json" };
+import pkg from "./package.json" with { type: "json" };
 const { version } = pkg;
 
 const NamedChunks = {
